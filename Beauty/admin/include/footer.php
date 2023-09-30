656e187e05c8d74b7613e432843107486998841c
yael de la cruz <brancruzdia23@gmail.com>
2023-09-29 19:43:10 -0600

    proyecto

---
+++ Beauty/admin/include/footer.php
@@ -0,0 +1,7 @@
+	<div class="footer">
+		<div class="container">
+			 
+
+			<b class="copyright">&copy; 2022 Golden Beauty - <a href="ebenitez12mtz@gmail.com"> </a> </b> Todos los derechos reservados.
+		</div>
+	</div>
